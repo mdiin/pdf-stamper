91fd727528bc04c9f2872dcf43adf211092f21b1
Matthias Diehn Ingesman <matthias@ingesman.dk>
2014-09-25 09:20:05 +0200

    Bump dependencies.

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
         :url "https://github.com/mdiin/pdf-stamper"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.xml "0.0.8"] ;; XML parsing
-                 [org.apache.pdfbox/pdfbox "1.8.6"] ;; PDF
-                 [potemkin "0.3.8"] ;; Code organisation
-                 [prismatic/schema "0.2.6"]
+                 [org.apache.pdfbox/pdfbox "1.8.7"] ;; PDF
+                 [potemkin "0.3.9"] ;; Code organisation
+                 [prismatic/schema "0.3.0"] ;; Template validations
                  ]
   :deploy-repositories [["releases" :clojars]])
 
