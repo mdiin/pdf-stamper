0f96d993bc4187503adb18b5026bc487187252ac
Matthias Diehn Ingesman <matthias@ingesman.dk>
2015-04-09 21:51:52 +0200

    Protocol doc strings.

--- src/pdf_stamper/protocols.clj
+++ src/pdf_stamper/protocols.clj
@@ -2,11 +2,12 @@
 
 ;; ## Token functions
 ;;
-;; Deriving information from tokens, such as a tokens width and height.
 (defprotocol Token
+  "Deriving information from tokens."
   (width [token formats context])
   (height [token formats context]))
 
 (defprotocol SelectToken
+  "Decision protocol: Is there room for `token` or not?"
   (select-token [token remaining-space formats context]))
 
