b337415e11e81e3737773c05a389b66cfbf93a59
Matthias Varberg Ingesman <matthias@ingesman.dk>
2018-03-03 21:21:08 +0100

    Require Clojure 1.9 and start using spec

--- project.clj
+++ project.clj
@@ -5,10 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/mdiin/pdf-stamper"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/data.xml "0.0.8"] ;; XML parsing
                  [org.apache.pdfbox/pdfbox "1.8.9"] ;; PDF
-                 [potemkin "0.3.10"] ;; Code organisation
+                 [potemkin "0.4.4"] ;; Code organisation
                  [prismatic/schema "1.0.4"] ;; Template validations
                  [org.clojure/test.check "0.7.0"]
                  ]
